cb257d2196ad18cf3aeace97af75df232a45ea18
Pascal M <oceanborn2@users.noreply.github.com>
2021-05-14 22:13:59 +0200

    Add files via upload

---
+++ pkghelper.jl
@@ -0,0 +1,61 @@
+import Pkg
+
+#using Pkg
+using Base
+using DocOpt
+using Gtk
+using Gtk.ShortNames
+
+mutable struct JuliaHelper
+    registries::Vector{Pkg.RegistrySpec}
+    server::String
+    depots::Vector{String}
+    dependencies::Dict{Base.UUID,Pkg.Types.PackageInfo}
+    main_depot::String
+end
+
+function JuliaHelper_init()
+    # self = JuliaHelper()
+    # self.server = Pkg.pkg_server()
+    # self.depots = Pkg.depots()
+    # self.main_depots = Pkg.depots1()
+    # #jh.registries = listRegistries()
+    # self.dependencies = Pkg.dependencies()
+    regspecs = []
+    return JuliaHelper(
+        regspecs,
+        Pkg.pkg_server(),
+        Pkg.depots(),
+        Pkg.dependencies(),
+        Pkg.depots1(),
+    )
+
+end
+
+function ensureUpToDate(self::JuliaHelper)
+    #Pkg.API.up()
+    Pkg.update()
+end
+
+function listRegistries(self::JuliaHelper)
+    #     return Registry.status()
+end
+#
+
+function displayInfo(self::JuliaHelper)
+    println("server : ", self.server)
+    println("registries: ")
+    allpkgs = self.dependencies.vals
+    for reg in allpkgs
+        if reg != undef
+            uuid::UUID = reg.uuid
+            name:String = reg.name
+            println(uuid, name)
+        end
+    end
+end
+
+# PackageSpec("Gtk") # => returns a PackageSpec record
+
+jh = JuliaHelper_init()
+displayInfo(jh)
